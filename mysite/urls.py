ac9ecb324dc4ef81b9bc25eca702dbbc2b9ebe99
Kota NAGAMIZO <kisslearon@gmail.com>
2022-05-02 13:11:15 +0900

    [new] new application hoge
    nagamizoがhogeアプリケーションを併設

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import include,path
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path('hoge/', include('hoge.urls')),
     path('admin/', admin.site.urls),
 ]
