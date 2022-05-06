5d66b6266e9749e1666d9d60868901c425a8432c
sender1999 <tksendamasato@gmail.com>
2022-05-06 09:35:10 +0900

    06まで行った

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,33 +1,32 @@
 import json
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-from asgiref.sync import async_to_sync  # async_to_sync() : 非同期関数を同期的に実行する際に使用する。
 
 # ChatConsumerクラス: WebSocketからの受け取ったものを処理するクラス
-class ChatConsumer( WebsocketConsumer ):
+class ChatConsumer(AsyncWebsocketConsumer):
 
     # WebSocket接続時の処理
-    def connect( self ):
+    async def connect(self):
         # グループに参加
         self.strGroupName = 'chat'
-        async_to_sync( self.channel_layer.group_add )( self.strGroupName, self.channel_name )
+        await self.channel_layer.group_add(self.strGroupName, self.channel_name)
 
         # WebSocket接続を受け入れます。
         # ・connect()でaccept()を呼び出さないと、接続は拒否されて閉じられます。
         # 　たとえば、要求しているユーザーが要求されたアクションを実行する権限を持っていないために、接続を拒否したい場合があります。
         # 　接続を受け入れる場合は、connect()の最後のアクションとしてaccept()を呼び出します。
-        self.accept()
+        await self.accept()
 
     # WebSocket切断時の処理
-    def disconnect( self, close_code ):
+    async def disconnect(self, close_code):
         # グループから離脱
-        async_to_sync( self.channel_layer.group_discard )( self.strGroupName, self.channel_name )
+        await self.channel_layer.group_discard(self.strGroupName, self.channel_name)
 
     # WebSocketからのデータ受信時の処理
     # （ブラウザ側のJavaScript関数のsocketChat.send()の結果、WebSocketを介してデータがChatConsumerに送信され、本関数で受信処理します）
-    def receive( self, text_data ):
+    async def receive(self, text_data):
         # 受信データをJSONデータに復元
-        text_data_json = json.loads( text_data )
+        text_data_json = json.loads(text_data)
 
         # メッセージの取り出し
         strMessage = text_data_json['message']
@@ -36,11 +35,11 @@ class ChatConsumer( WebsocketConsumer ):
             'type': 'chat_message', # 受信処理関数名
             'message': strMessage, # メッセージ
         }
-        async_to_sync( self.channel_layer.group_send )( self.strGroupName, data )
+        await self.channel_layer.group_send(self.strGroupName, data)
 
     # 拡散メッセージ受信時の処理
     # （self.channel_layer.group_send()の結果、グループ内の全コンシューマーにメッセージ拡散され、各コンシューマーは本関数で受信処理します）
-    def chat_message( self, data ):
+    async def chat_message(self, data):
         data_json = {
             'message': data['message'],
         }
@@ -48,4 +47,4 @@ class ChatConsumer( WebsocketConsumer ):
         # WebSocketにメッセージを送信します。
         # （送信されたメッセージは、ブラウザ側のJavaScript関数のsocketChat.onmessage()で受信処理されます）
         # JSONデータをテキストデータにエンコードして送ります。
-        self.send( text_data=json.dumps( data_json ) )
+        await self.send(text_data=json.dumps(data_json))
